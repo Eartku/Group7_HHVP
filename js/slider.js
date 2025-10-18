f35e0f8ad5a0add49693ee5273bfd976a86e1477
Huylore <earth4.5my@gmail.com>
2025-10-18 10:45:19 +0700

    Reapply "UI updated"
    This reverts commit c2d4da1e4614d4d0ee4bd3d15499ca1a1df091f2.

--- js/slider.js
+++ js/slider.js
@@ -6,3 +6,24 @@ setInterval(() => {
   current = (current + 1) % slides.length;
   slides[current].classList.add('active');
 }, 3000);
+
+
+const isLoggedIn = false;
+
+function checkLogin(event) {
+  if (!isLoggedIn) {
+    event.preventDefault(); // chặn chuyển trang
+
+    // Bước 1: cảnh báo ban đầu
+    alert("⚠️ Bạn phải đăng nhập trước khi xem chi tiết!");
+
+    // Bước 2: hỏi người dùng có muốn đăng nhập không
+    const confirmLogin = confirm("Bạn có muốn đến trang đăng nhập không?");
+    
+    if (confirmLogin) {
+      // Nếu người dùng bấm OK → chuyển sang trang đăng nhập
+      window.location.href = "login.html";
+    }
+  }
+}
+
