dcd8ec517a052d5a76ff0a21762b0aceb7eb4e37
phungvumao-lgtm <phungvumao@gmail.com>
2026-03-12 07:39:30 +0700

    12/03/2026 - header

--- bonsai/admin_includes/header.php
+++ bonsai/admin_includes/header.php
@@ -13,88 +13,79 @@ if ($result && $result->num_rows > 0) {
     }
 }
 
-/* ===== LẤY AVATAR USER ===== */
-$userAvatar = "../images/user.png";
 
-if (isset($_SESSION['user'])) {
-    $userId = $_SESSION['user']['id'];
-
-    $stmt = $conn->prepare("SELECT avatar FROM users WHERE id=?");
-    $stmt->bind_param("i", $userId);
-    $stmt->execute();
-    $res = $stmt->get_result();
-    $u = $res->fetch_assoc();
-
-    if (!empty($u['avatar'])) {
-        $userAvatar = "../uploads/avatars/" . htmlspecialchars($u['avatar']);
-    }
+$avatar = "../images/avatar.svg";
+if (!empty($_SESSION['admin']['avatar'])) {
+    $avatar = "../uploads/" . $_SESSION['admin']['avatar'];
 }
+
 ?>
 
 <!-- Start Header -->
 <nav class="custom-navbar navbar navbar-expand-md navbar-dark bg-dark">
-        <div class="collapse navbar-collapse" id="navbarsFurni">
+      
+<div class="container">
+    <!-- LOGO -->
+    <a class="navbar-brand">BonSai<span>🌱</span></a>
 
-            <ul class="custom-navbar-nav navbar-nav ms-auto">
+    <!-- MOBILE BUTTON -->
+    <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarsFurni">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-                <li>
-                    <a href="customermanage.php"
-                       class="nav-link active-import"
-                       style="font-size:19px">
-                        KHÁCH HÀNG
-                    </a>
-                </li>
 
-                <li>
-                    <a href="admin_type.php"
-                       class="nav-link hover-text-glow"
-                       style="font-size:14px">
-                        LOẠI SẢN PHẨM
-                    </a>
-                </li>
 
-                <li>
-                    <a href="admin_product.php"
-                       class="nav-link hover-text-glow"
-                       style="font-size:14px">
-                        SẢN PHẨM
-                    </a>
-                </li>
+      <ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
+            <li>
+              <a href="admin/customermanage.html" class="hover-text-glow-small"
+                  style="font-size: 13px"
+                >KHÁCH HÀNG</a
+              >
+            </li>
+            <li>
+              <a href="admin/admin_type.html" class="hover-text-glow-small"
+                 style="font-size: 13px"
+                >LOẠI SẢN PHẨM</a
+              >
+            </li>
+            <li>
+              <a  href="admin_product/sshop.php" class="hover-text-glow-small"
+                 style="font-size: 13px"
+                >SẢN PHẨM</a
+              >
+            </li>
+             <li>
+              <a href="admin_importproduct/adminipd.php" class="hover-text-glow-small"
+                 style="font-size: 13px"
+                >NHẬP SẢN PHẨM</ax
+              >
+            </li>
+             <li>
+              <a href="admin/admin_sellingprice.html" class="hover-text-glow-small"
+                 style="font-size: 13px"
+                >GIÁ BÁN</a
+              >
+            </li>
+            <li>
+              <a  href="admin/admin_order.html" class="hover-text-glow-small"
+                 style="font-size: 13px"
+                >ĐƠN HÀNG</a
+              >
+            </li>
+            <li>
+              <a href="admin/admin_stock1.html" class="hover-text-glow-small"
+                 style="font-size: 13px"
+                >KHO</a
+              >
+            </li>
+          </ul>
 
-                <li>
-                    <a href="admin_importproduct.php"
-                       class="nav-link hover-text-glow"
-                       style="font-size:14px">
-                        NHẬP SẢN PHẨM
-                    </a>
-                </li>
 
-                <li>
-                    <a href="admin_sellingprice.php"
-                       class="nav-link hover-text-glow"
-                       style="font-size:14px">
-                        GIÁ BÁN
-                    </a>
-                </li>
-
-                <li>
-                    <a href="admin_order.php"
-                       class="nav-link hover-text-glow"
-                       style="font-size:14px">
-                        ĐƠN HÀNG
-                    </a>
-                </li>
-
-                <li>
-                    <a href="admin_stock1.php"
-                       class="nav-link hover-text-glow"
-                       style="font-size:14px">
-                        KHO
-                    </a>
-                </li>
-
-            </ul>
-
+        <!-- ICON -->
             <ul class="custom-navbar-cta navbar-nav ms-4 align-items-center">
 
                 <li class="nav-item">
@@ -119,10 +110,10 @@ if (isset($_SESSION['user'])) {
                 </li>
 
             </ul>
+    
 
-        </div>
-
-    </div>
+</div>
+   
 
 </nav>
 <!-- End Header -->
